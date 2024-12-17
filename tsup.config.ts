b6278108983bda157ec073fe406e4d26dae52b9b
lijspoop <lijs.poop@gmail.com>
2024-12-17 15:46:44 +0800

    add iife support for importing to userscript

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,12 +1,36 @@
 import { defineConfig } from 'tsup';
 
-export default defineConfig({
-  entry: ['src/**/*.ts'],
-  dts: true,
-  clean: true,
-  keepNames: true,
-  minify: true,
-  target: 'es2022',
-  format: ['cjs', 'esm'],
-  bundle: false,
-});
+export default defineConfig([
+  {
+    entry: ['src/index.ts'],
+    dts: true,
+    clean: true,
+    keepNames: true,
+    bundle: true,
+    minify: true,
+    target: 'es2022',
+    globalName: 'KodikWrapper',
+    format: ['cjs', 'esm', 'iife'],
+    outExtension({ format, pkgType }) {
+      console.log(pkgType, format);
+      return {
+        js: `.${format === 'iife' ? 'global' : format}.prod.js`,
+      }
+    },
+  },
+  {
+    entry: ['src/index.ts'],
+    dts: true,
+    keepNames: true,
+    bundle: true,
+    target: 'es2022',
+    globalName: 'KodikWrapper',
+    format: ['cjs', 'esm', 'iife'],
+    outExtension({ format, pkgType }) {
+      console.log(pkgType, format);
+      return {
+        js: `.${format === 'iife' ? 'global' : format}.js`,
+      }
+    },
+  }
+]);
